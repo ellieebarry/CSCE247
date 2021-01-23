2833c5e36b4a41b97ab08a5bac120fdd4c030a52
Ellie <ellie455@gmail.com>
2021-01-23 15:52:54 -0500

    save changes

--- ObserverDesignPattern/Golfer.java
+++ ObserverDesignPattern/Golfer.java
@@ -1,12 +1,37 @@
 package ObserverDesignPattern;
 
-public class Golfer {
-    
-    public void registerObserver(Observer observer){
+import java.util.*;
 
+public class Golfer implements Subject {
+
+    private ArrayList<Observer> observers = new ArrayList<Observer>();
+    private String name;
+
+    public Golfer(String name) {
+        this.name = name;
+        // this.enterScore(int strokes, int par);
     }
 
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
 
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
 
+    public void notifyObservers(int strokes, int par) {
+        for (Observer observer : observers) {
+            observer.update(strokes, par);
+        }
+    }
+
+    public void enterScore(int strokes, int par) {
+        notifyObservers(strokes, par);
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
