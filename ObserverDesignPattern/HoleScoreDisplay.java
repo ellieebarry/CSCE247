d4c8ee7b652b4326554aa407fd18660955354f8d
Ellie <ellie455@gmail.com>
2021-01-23 16:44:36 -0500

    Save changes

--- ObserverDesignPattern/HoleScoreDisplay.java
+++ ObserverDesignPattern/HoleScoreDisplay.java
@@ -1,5 +1,19 @@
 package ObserverDesignPattern;
 
-public class HoleScoreDisplay {
-    
+public class HoleScoreDisplay implements Observer{
+    private Subject golfer;
+    private int strokes;
+    private int par;
+    public HoleScoreDisplay(Subject golfer){
+        this.golfer = golfer;
+    }
+    public void update(int strokes, int par){
+        this.strokes = strokes;
+        this.par = par;
+    }
+
+    private void displayCurrentScore(){
+        
+    }
+
 }
