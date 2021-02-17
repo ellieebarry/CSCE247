ef5d3f7258a14faa05fccda052f65dc99dc3f7d5
Ellie <ellie455@gmail.com>
2021-02-17 14:44:27 -0500

    final changes

--- SingletonDesignPattern/Library.java
+++ SingletonDesignPattern/Library.java
@@ -2,42 +2,65 @@ package SingletonDesignPattern;
 
 import java.util.*;
 
+/**
+ * Class that creates a library of books
+ * @author Eleanor Barry
+ */
+
 public class Library {
 
-    private HashMap<String,Integer> books;
+    private HashMap<String, Integer> books;
     private static Library library;
 
-    private Library(){
-        books = new HashMap<String,Integer>();
-        // library?
+    /**
+     * Constructor method that sets books up as a HashMap and prints that the
+     * library is being created.
+     */
+    private Library() {
+        books = new HashMap<String, Integer>();
         System.out.println("Creating our Library. Time to begin reading.");
     }
 
-    public static Library getInstance(){
-        if(library == null){
+    /**
+     * Method that returns the library and sets up the library if it is null.
+     * @return a Library of books
+     */
+    public static Library getInstance() {
+        if (library == null) {
             System.out.println("");
             library = new Library();
         }
         return library;
     }
 
-    public boolean checkoutBook(String bookName){
-        if(books.containsKey(bookName)){
-            if(books.get(bookName) > 1){
+    /**
+     * Method to remove one copy of that book from the library.
+     * @param bookName String that is the value of the book being removed
+     * @return Boolean of whether the book could be checked out or not
+     */
+    public boolean checkoutBook(String bookName) {
+        if (books.containsKey(bookName)) {
+            if (books.get(bookName) > 0) {
                 books.put(bookName, books.get(bookName) - 1);
+                System.out.println(bookName + " was successfully checked out");
+                return true;
             } else {
-                books.remove(bookName);
+                System.out.println("Sorry " + bookName + " is not in stock");
+                return false;
             }
-            System.out.println(bookName + " was successfully checked out");
-            return true;
-        } else{
-            System.out.println("Sorry " + bookName + " is not in stock");
+        } else {
             return false;
         }
     }
 
-    public void checkInBook(String bookName, int numToAdd){
-        if(books.containsKey(bookName)){
+    /**
+     * Method to add a copy of a book to the library.
+     * @param bookName String that is the value of the book
+     * @param numToAdd Integer that is the key to add to the original key of the
+     *                 value.
+     */
+    public void checkInBook(String bookName, int numToAdd) {
+        if (books.containsKey(bookName)) {
             books.put(bookName, books.get(bookName) + numToAdd);
         } else {
             books.put(bookName, numToAdd);
@@ -45,12 +68,16 @@ public class Library {
         System.out.println(bookName + " was added to the library");
     }
 
-    public void displayBooks(){
-        for (Map.Entry<String,Integer> entry : books.entrySet()) {
+    /**
+     * Method that goes through the library and prints the key and value of each book.
+     */
+    public void displayBooks() {
+        System.out.println();
+        System.out.println("Inventory:");
+        for (Map.Entry<String, Integer> entry : books.entrySet()) {
             System.out.println("- " + entry.getKey() + ", copies: " + entry.getValue());
-            
-          }
+
+        }
     }
 
 }
-
